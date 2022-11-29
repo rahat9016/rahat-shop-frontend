6f0a21131bc88484ae676192fc5b34c0061e18fe
Minhajur Rohoman <minhajurrohoman9016@gmail.com>
2022-11-30 03:36:17 +0600

    best product page implement find by sold

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { InitiateDataFunc } from "../../action/InitiateData";
 import HeroSection from "../../Components/HeroSection/HeroSection";
 import Layout from "../../Components/Layout/Layout";
 import MenuSection from "../../Components/Menu/MenuSection";
-
+import "./style.css";
 const Home = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.AllProductsData);
+  console.log(products);
+  useEffect(() => {
+    dispatch(InitiateDataFunc({ sort: "sold", order: "desc", limit: 5 }));
+  }, [dispatch]);
   return (
     <Layout home>
       <div className="container mx-auto">
@@ -13,7 +21,32 @@ const Home = () => {
           <HeroSection />
         </div>
       </div>
-      <div className="flex justify-between"></div>
+      <div className="container mx-auto">
+        <div className="productItemContainer p-2">
+          {products.loading ? (
+            <p>Loading...</p>
+          ) : (
+            products?.products?.map((product, index) => {
+              return (
+                <div
+                  key={product._id}
+                  className={`p-3  rounded-3xl ${"proImg" + index}`}
+                >
+                  <div className={`flex justify-center `}>
+                    <img src={product?.productPictures[0].img} alt="" />
+                  </div>
+                  <div>
+                    <h1 className="mb-1">{product.name}</h1>
+                    <p className="text-sm">
+                      {product.description.slice(0, 150)}
+                    </p>
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
     </Layout>
   );
 };
